7c4a99209ddec57ede7e948a096419e16006a393
Mushahid111 <85597438+Mushahid111@users.noreply.github.com>
2024-04-01 16:25:18 +0530

    Update script.js
    added emoji

--- test/script.js
+++ test/script.js
@@ -7,7 +7,7 @@ function startPrank() {
       const randomColor = colors[Math.floor(Math.random() * colors.length)]; // Randomly select a color
       const style = `background-color: ${randomColor}; color: white; font-size: 18px; padding: 20px; border-radius: 10px;`;
       
-      alert("You've been pranked! Happy April Fools' Day!");
+      alert("You've been pranked! Happy April Fools' Day! 🎁");
       const alertBoxes = document.querySelectorAll('.alert');
       alertBoxes.forEach(alertBox => alertBox.setAttribute('style', style));
     }, i * 2000); // Change the delay (2000ms = 2 seconds) between alerts if needed
