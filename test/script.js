8deeef6c51e4b5e6602654b319695f719243b261
Mushahid111 <85597438+Mushahid111@users.noreply.github.com>
2024-04-01 16:24:18 +0530

    Update script.js
    nono

--- test/script.js
+++ test/script.js
@@ -1,10 +1,4 @@
-// function startPrank() {
-//   for (let i = 0; i < 50; i++) {
-//     setTimeout(() => {
-//       alert("You've been pranked! Happy April Fools' Day!");
-//     }, i * 2000); // Change the delay (2000ms = 2 seconds) between alerts if needed
-//   }
-// }
+
 function startPrank() {
   const colors = ['#ff6347', '#ffa07a', '#20b2aa', '#9370db', '#ff7f50']; // Array of colors for alerts
 
